7d032e77005e0b76ae4861eaa620a3518c9a5e69
dikwhyudi <49176878+dikwhyudi@users.noreply.github.com>
2019-04-02 09:49:13 +0700

    Update Main.java

--- Fibonacci/src/com/company/Main.java
+++ Fibonacci/src/com/company/Main.java
@@ -5,21 +5,33 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         Scanner d = new Scanner(System.in);
-        System.out.println("Flibonaci");
+        System.out.println("Fibonaci");
         System.out.print("Masukan : ");
         int angka = d.nextInt();
-        long array[] = new long[angka];
+        System.out.println("jumlah : "+fibonacci(angka));
+//        long array[] = new long[angka];
+//
+//        array[0] = 0;
+//        array[1] = 1;
+//
+//        for (int i = 2; i < angka ; i++) {
+//            array[i] = array[i-1] + array[1+2];
+//        }
+//        for (int i = 0; i <angka ; i++) {
+//            System.out.print(array[i]+" ");
 
-        array[0] = 0;
-        array[1] = 1;
 
-        for (int i = 2; i < angka ; i++) {
-            array[i] = array[i-1] + array[1+2];
-        }
-        for (int i = 0; i <angka ; i++) {
-            System.out.print(array[i]+" ");
-        }
     }
+
+    static int fibonacci(int angka) {
+        if ((angka == 1) || (angka == 2))
+                return (1);
+        else
+            return (fibonacci(angka - 1)+fibonacci(angka - 2));
+
+    }
+
 }
+
